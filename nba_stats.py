539cda1dbe3169ecbb09dc0f1f8f8c11abbf9b6a
tkm4842 <35785063+tkm4842@users.noreply.github.com>
2018-05-03 15:21:51 -0700

    Update nba_stats.py

--- nba_stats.py
+++ nba_stats.py
@@ -1,34 +1,38 @@
-#from nba_py import player
-from selenium import webdriver
+
 from bs4 import BeautifulSoup
-import pandas as pd
-import os
-from string import ascii_lowercase
+from collections import OrderedDict
 import cPickle as pickle
 import csv
-import re
+import datetime as dt
 from datetime import timedelta as td
 import dill
+import os
+import pandas as pd
+import re
+import requests
+from selenium import webdriver
+from sklearn.dummy import DummyRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from string import ascii_lowercase
 
+pd.options.mode.chained_assignment = None
 
 player_base_url = 'https://www.basketball-reference.com/players/'
-
 
 def getPlayerURLdict(new=False):
     if new==True:
         all_names = dict()
         chromedriver = os.path.expanduser('~/Downloads/chromedriver')
         driver = webdriver.Chrome(chromedriver)
-
         for letter in ascii_lowercase:
             letter_url = player_base_url+letter
-
             driver.get(letter_url)
             htmlSource = driver.page_source
             soup = BeautifulSoup(htmlSource, 'html.parser')
             current_names = soup.findAll('strong')
             names = []
-            for i, n in enumerate(current_names):
+            for _, n in enumerate(current_names):
                 name_data = n.children.next()
                 try:
                     names.append((name_data.contents[0], 'http://www.basketball-reference.com' + name_data.attrs['href']))
@@ -41,7 +45,7 @@ def getPlayerURLdict(new=False):
         for key, val in all_names.items():
             w.writerow([key, val])
         return all_names
-        driver.close()
+        
     else:
         reader=  csv.reader(open('playerURL.csv', 'r'))
         d = {}
@@ -88,13 +92,10 @@ def getTeamURLdict(new=False):
 
 
 
-
-
 def getURL(playerName):
     lastnameletter = playerName.split(' ')[1][0].lower()
     url = player_base_url+lastnameletter
     return url
-
 
 def addhomeaway(df):
     df['Away']=0
@@ -115,12 +116,37 @@ def addDaysRest(df):
             df.loc[i, 'daysRest'] = (date-prevdate).total_seconds()/(24*3600.)
     return df
 
+def addTeamGameLog(df,season):
+    # 3 letter team code
+    team=str(df['Tm'].unique()[0])
+    team_dict=getTeamURLdict()
+
+    # convert to full team name to access pkl
+    fullteam_name=[k for k,v in team_dict.iteritems() if team in v][0]
+    teampkl_source = 'pkl_obj/' +fullteam_name.replace(" ", "") + '.dill'
+    team_obj=dill.load(open(teampkl_source, 'r'))
+
+    # get team game log
+    teamlog=team_obj.game_log[season]
+
+    # rename pts to be consistent with other columns
+    team_log=teamlog.rename(columns={'Pts':'PTS'})
+    cols = ['Rk','PTS','FG','FGA','3P','3PA','FT','FTA','TRB','AST','STL','BLK','TOV']
+
+    # cats distinguised by _x for player, _y for team
+    new_player_log=pd.merge(df, team_log[cols], on=['Rk'], how='left')
+
+    # rename player cols to original i.e. drop _x
+    new_player_log.columns=[col.replace('_x','') if '_x' in col else col for col in new_player_log.columns ]
+    return new_player_log
+
 def getTeamSchedule(team,season):
     teamscheduleURL='https://www.basketball-reference.com/teams/'+team+'/'+str(season)+'_games.html'
     chromedriver = os.path.expanduser('~/Downloads/chromedriver')
     driver = webdriver.Chrome(chromedriver)
     driver.get(teamscheduleURL)
     htmlSource = driver.page_source
+ 
     soup = BeautifulSoup(htmlSource, 'html.parser')
     table = soup.find('div', class_="overthrow table_container", id="div_games")
     df = pd.read_html(table.prettify())[0]
@@ -132,54 +158,120 @@ def getTeamSchedule(team,season):
     return df
 
 
-class player_scraper():
+def getPlayerPosition(soup):
+    line=[x.get_text() for x in soup.findAll('p') if 'Position' in x.get_text()][0]
+    pos_dict = OrderedDict()
+    pos_dict['Point Guard']='PG'
+    pos_dict['Shooting Guard']='SG'
+    pos_dict['Small Forward']='SF'
+    pos_dict['Power Forward']='PF'
+    pos_dict['Center']='C'
+
+    p=''
+    for pos in pos_dict:
+        if pos in line:
+            if p=='':
+                p=pos_dict[pos]
+            else:
+                p+=', '+pos_dict[pos]
+    return p        
+
+class playerScraper():
     def __init__(self,playerName):
         self.playerName=playerName
-        self.game_log, self.pic, self.team = self.getPlayerData()
+        self.getPlayerData()
 
     def getPlayerData(self):
-        #filename = 'PlayerGameLogs/'+self.playerName.replace(" ", "") + '_' + str(self.season) + 'GameLog.csv'
-
         playerURLdict = getPlayerURLdict()
         playerURL = playerURLdict[self.playerName]
-        chromedriver = os.path.expanduser('~/Downloads/chromedriver')
-        driver = webdriver.Chrome(chromedriver)
         url = playerURL
         url = re.sub('\.html$', '', url)
 
         seasons=[str(i) for i in xrange(2018,2019,1)]
         game_log = dict()
+        models = dict()
+              
         for season in seasons:
-
+            # Beautiful Soup on player gamelog html page
             playerGameLogURL = url+'/gamelog/'+season+'/'
-            driver.get(playerGameLogURL)
-            htmlSource = driver.page_source
-            soup = BeautifulSoup(htmlSource, 'html.parser')
-
-            # Game Logs
+            page = requests.get(playerGameLogURL)
+            soup = BeautifulSoup(page.content, 'html.parser')    
             table = soup.find('div', class_="overthrow table_container", id="div_pgl_basic")
+            
+            # Game Logs
             df = pd.read_html(table.prettify())[0]
             df = df[ (pd.notnull(df['G'])) & (df['G']!='G') ]
 
+            # Add attributes: home/away, days rest, team game log (cat allowed by opponent)
             df = addhomeaway(df)
             df = addDaysRest(df)
-
+            df = addTeamGameLog(df, season)
             game_log[season]=df
+            
+            # Regression Models
+            models[season] = addRegressionModels(df)
 
+        self.game_log=game_log
+        self.models=models
+        
         # Player profile pic
         pic = soup.find('img', itemscope="image")
-        pic_url = str(pic['src'])
+        self.pic_url = str(pic['src'])
 
         # Team
-        team = str(df['Tm'].unique()[0])
+        self.team = str(df['Tm'].unique()[0])
+        
+        # Height, Weight, Age, position
+        height=soup.find("span", itemprop="height")
+        self.height=str(height.get_text())
+        
+        weight=soup.find("span", itemprop="weight")
+        self.weight=str(weight.get_text())
+        
+        bdate=soup.find("span", itemprop="birthDate")['data-birth']
+        self.age=str(int((dt.datetime.now()-dt.datetime.strptime(bdate,'%Y-%m-%d')).days/365.0))
+        
+        self.position=getPlayerPosition(soup)
+        
+        
+        # Close driver
+        #driver.close()
+        
 
-        driver.close()
 
-        return game_log, pic_url, team
+def addRegressionModels(data):
+    cats= ['FG','FGA','3P','3PA','FT','FTA','TRB','AST','STL','BLK','TOV','PTS']
+    features=['G','Away','daysRest'] # will include opponent rating by category when iterating through cats
+    
+    # Regression Models
+    models=dict()
+    models['dummyReg']=dict()
+    models['RFG']=dict()
+    for cat in cats:
+        # add opponent rating per category
+        temp_features=[] # clear everytime
+        temp_features+=features
+        temp_features.append(cat+'_y')
 
+        # convert to numeric objects
+        X=data[temp_features].apply(pd.to_numeric)
+        yi=data[cat].apply(pd.to_numeric)
 
+        ## Dummy Regressor to return season averages regardless of predictions
+        dummyreg=DummyRegressor(strategy='mean')
+        dummyreg.fit(X,yi)
+        models['dummyReg'][cat]=dummyreg
+        
+        ## Random Forest Regressor
+        ### may need to use grid search to optimize/tune hyperparameters
+        RFG=RandomForestRegressor(min_samples_leaf=9)
+        RFG.fit(X,yi)
+        models['RFG'][cat]=RFG
+    
+    return models
+    
 
-class team_scraper():
+class teamScraper():
     def __init__(self,teamName):
         self.teamName=teamName
         self.game_log, self.pic, self.roster= self.getTeamData()
@@ -189,8 +281,8 @@ class team_scraper():
 
         teamURLdict = getTeamURLdict()
         teamURL = teamURLdict[self.teamName]
-        chromedriver = os.path.expanduser('~/Downloads/chromedriver')
-        driver = webdriver.Chrome(chromedriver)
+        #chromedriver = os.path.expanduser('~/Downloads/chromedriver')
+        #driver = webdriver.Chrome(chromedriver)
         url = 'https://www.basketball-reference.com'+teamURL
         #url = re.sub('\.html$', '', url)
 
@@ -205,10 +297,10 @@ class team_scraper():
 
         for season in seasons:
             teamGameLogURL = url+season+'/gamelog'
+            page = requests.get(teamGameLogURL)
+            soup = BeautifulSoup(page.content, 'html.parser')    
 
-            driver.get(teamGameLogURL)
-            htmlSource = driver.page_source
-            soup = BeautifulSoup(htmlSource, 'html.parser')
+            #soup = BeautifulSoup(htmlSource, 'html.parser')
 
             # Game Logs
             table = soup.find('div', class_="overthrow table_container", id="div_tgl_basic")
@@ -217,12 +309,11 @@ class team_scraper():
             df.columns=cols
             df = df[ (pd.notnull(df['G'])) & (df['G']!='G') ]
 
-
             df = addhomeaway(df)
             df = addDaysRest(df)
-
+            
             game_log[season]=df
-
+            
 
         # Team profile pic
         pic = soup.find('img', class_="teamlogo")
@@ -232,7 +323,7 @@ class team_scraper():
         # Roster
         roster=None
 
-        driver.close()
+        #driver.close()
         return game_log, pic_url, roster
 
 
